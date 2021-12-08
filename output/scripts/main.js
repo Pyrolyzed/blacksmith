b94b6d70869b0609d6e7deae4edeceeac43bdc7c
Pyrolyzed <blevinse32@gmail.com>
2021-12-07 19:50:32 -0600

    this shit no work

--- output/scripts/main.js
+++ output/scripts/main.js
@@ -1,3 +1,11 @@
-const version = require('../../package.json').version;
-
-document.getElementById('test').innerHTML = `Version ${version}`
+const fs = require('fs')
+var version
+var packageFile = fs.readFile("../../package.json", "utf8", function(error, data){
+    if(err){
+      console.log(err)
+    }
+    version = data
+})
+version = JSON.parse(version)
+version = version.version
+console.log(version);
