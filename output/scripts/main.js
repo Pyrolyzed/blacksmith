78bda4a9c64df73895a82851dd3fb92db24158df
Pyrolyzed <blevinse32@gmail.com>
2021-12-07 20:01:34 -0600

    Fixes

--- output/scripts/main.js
+++ output/scripts/main.js
@@ -1,11 +1,6 @@
 const fs = require('fs')
 var version
-var packageFile = fs.readFile("../../package.json", "utf8", function(error, data){
-    if(err){
-      console.log(err)
-    }
-    version = data
-})
-version = JSON.parse(version)
-version = version.version
-console.log(version);
+var packageFile = fs.readFileSync("package.json", { encoding: "utf-8", flag: "r" })
+var version = JSON.parse(packageFile).version
+
+document.getElementById('test').innerHTML = version;
